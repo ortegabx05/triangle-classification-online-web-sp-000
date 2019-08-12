b092bdac65c244100897842e4bcfc260ecc44691
jessi ortega <ortegabx05@gmail.com>
2019-08-12 02:33:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,6 +22,7 @@ class Triangle
     [a, b, c].each { |s| real_triangle << false if s <= 0 }
     raise TriangleError if real_triangle.include?(false)
   end
+end
 
 class TriangleError < StandardError
   def message
